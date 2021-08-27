70bee6f99584717885322523d89946c1e5393bc3
lp33720 <51269245+lp33720@users.noreply.github.com>
2021-08-27 18:27:07 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+import json
+from concurrent.futures import process
+from pathlib import Path
+import discord
+
+##############
+
+# PATH = Path("/home/aurnytoraink/Bot/Coiffeur")
+from discord.ext import commands
+from discord.ext.commands import bot
+
+PATH = Path("E:\Dev\Bot_QuoiFeur")
+# Insérer l'adresse où se situe votre dossier
+
+TOKEN = json.loads(open(PATH / "config.json", 'rb').read())["token"]
+
+#############
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+    # Setting `Playing ` status
+    bot.change_presence(activity=discord.Game(name="Manger Drakior"))
+
+
+@client.event
+async def on_message(message):
+    # Évite que le bot intercepte ces propres messages
+    if message.author == client.user:
+        return
+
+    # Prise en charge des majuscules et mininuscules
+    # + les textes déformés comme "qUoiiiiiiiiiiiiiiiii ?"
+    if message.content.lower().endswith('quoi'):
+        await message.channel.send("FEUR!!!!")
+    if message.content.lower().endswith('quoi ?'):
+        await message.channel.send("FEUR!!!!")
+    if message.content.lower().endswith('quoi?'):
+        await message.channel.send("FEUR!!!!")
+
+
+client.run(TOKEN)
